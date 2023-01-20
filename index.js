f7155a0af5c3fb2534d145fda42ebbf8af7a6333
PallFebrian <pallfebrian9@gmail.com>
2023-01-20 13:25:23 +0700

    express

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
 const express = require('express');
 const app = express();
-require('dotenv').config() 
+require('dotenv').config();
 const port = process.env.PORT || 1224;
 const router = require('./src/routes');
-const authMiddleWare = require("./src/routes/middleware/authMiddleWare")
-const console1 = require('./src/routes/middleware/console1')
+const { sequelize } = require('./src/models');
+const authMiddleWare = require('./src/routes/middleware/authMiddleWare');
+const console1 = require('./src/routes/middleware/console1');
 // const console2 = require('./src/routes/middleware/console2')
 
-
-app.use(express.json())
-app.use(express.static("src/storage/uploads"))
+app.use(express.json());
+app.use(express.static('src/storage/uploads'));
 // app.use(console2)
-app.use(console1)
+app.use(console1);
 // app.use(authMiddleWare)
 app.use(router);
-app.listen(port, () =>
-  console.log(`Server berjalan di http://localhost:${port}`)
+app.listen(
+  port,
+  async () => {
+    try {
+      await sequelize.authenticate();
+      console.log('Koneksi telah berhasil dibuat.');
+    } catch (error) {
+      console.error('Tidak dapat terhubung ke database.', error);
+    }
+  }
+
+  // console.log(`Server berjalan di http://localhost:${port}`)
 );
